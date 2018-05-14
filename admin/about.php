2ac486e0db4446a1b590777a5e37af1c7232bf04
egiwarrior21 <37869610+egiwarrior21@users.noreply.github.com>
2018-05-14 12:18:11 +0700

    Add files via upload

---
+++ admin/about.php
@@ -0,0 +1,212 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>BuduBagak LuxuryHotel </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900|Rubik:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <header role="banner">
+     
+      <nav class="navbar navbar-expand-md navbar-dark bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="index.html">BuduBagak LuxuryHotel</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse navbar-light" id="navbarsExample05">
+            <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+				<li class="nav-item">
+                <a class="nav-link" href="rooms.php">Rooms</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="about.php">About</a>
+              </li>
+               <li class="nav-item cta">
+                <a class="nav-link" href="admin.php"><span>Admin</span></a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- END header -->
+
+    <section class="site-hero site-hero-innerpage overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/big_image_1.jpg);">
+      <div class="container">
+        <div class="row align-items-center site-hero-inner justify-content-center">
+          <div class="col-md-12 text-center">
+
+            <div class="mb-5 element-animate">
+              <h1>About BuduBagak Luxury Hotel</h1>
+              <p>Discover our world's #1 Luxury Room For VIP.</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+
+    <section class="site-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <div class="heading-wrap  element-animate">
+            
+              <h2 class="heading">Our Story</h2>
+              <p class="">Our story start in Bandar Lampung exactly in the rented house at Tanjung Karang. it starts when we met at Lampung University. We talk about rented house and holiday, because of that we have an idea to make a place for someone to have a good holiday. </p>
+
+              <p>After that meeting our journeys begin with unexpected experience, we meet people from another city, country and many more. find a place for the hotel, make a web for advertisement and begin to cooperate with another company. </p>
+
+              <p>1 week after finishing the web we share it to friend, to people, from chat to chat, from televesion to make everyone know about this Luxury Hotel. To give information for all people around the world know that this hotel is the best place to visit for your holiday.</p>
+
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-7">
+            <img src="pelek.jpg" alt="Image placeholder" class="img-md-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+
+
+   
+   
+    
+    <section class="site-section bg-light">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-12 heading-wrap text-center">
+              <h2 class="heading">Our Staff</h2>
+          </div>
+        </div>
+        <div class="row ">
+          <div class="col-md-4">
+            <div class="post-entry">
+              <img src="images/egi2.jpg" alt="Image placeholder" class="img-fluid">
+              <div class="body-text">
+                <div class="category">Designer</div>
+                <h3 class="mb-3"><a href="#">Haravy Egi Pratama</a></h3>
+                <p class="mb-4">1617051085</p>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="post-entry">
+              <img src="images/adaww.jpg" alt="Image placeholder" class="img-fluid">
+              <div class="body-text">
+                <div class="category">Programmer</div>
+                <h3 class="mb-3"><a href="#">Febryoga Saputra</a></h3>
+                <p class="mb-4">1657051014</p>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="post-entry">
+              <img src="images/bowo2.jpg" alt="Image placeholder" class="img-fluid">
+              <div class="body-text">
+                <div class="category">Marketing</div>
+                <h3 class="mb-3"><a href="#">Arlis Putra Prabowo</a></h3>
+                <p class="mb-4">1617051128</p>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section-cover" data-stellar-background-ratio="0.5" style="background-image: url(images/img_5.jpg);">
+      <div class="container">
+        <div class="row justify-content-center align-items-center intro">
+          <div class="col-md-9 text-center element-animate">
+            <h2>Relax and Enjoy your Holiday</h2>
+            <p class="lead mb-5">Holiday is not perfect if you are not spend your holiday in hotel. But if you want more joy in your holiday come to BuduBagak Luxury Hotel. Come and we serve you like you live in the heaven in the middle of meadow.</p>
+       
+            <video width="600" height="280" controls="controls" preload="none" onclick="this.play()">
+ <source type="video/mp4" src="adaw.mp4">
+</video>
+        </div>
+
+    </div>
+</div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+   
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-4">
+            <h3>Phone Support</h3>
+            <p>24/7 Call us now.</p>
+            <p class="lead"><a href="tel://">+6281 1791 7298</a></p>
+          </div>
+          <div class="col-md-4">
+            <h3>Connect With Us</h3>
+            <p>We are socialized. Follow us</p>
+            <p>
+              <a href="https://www.facebook.com/haravi.pratama" class="pl-0 p-3"><span class="fa fa-facebook"></span></a>
+              <a href="https://twitter.com/egiwarrior21" class="p-3"><span class="fa fa-twitter"></span></a>
+              <a href="https://www.instagram.com/haravy_egi/" class="p-3"><span class="fa fa-instagram"></span></a>
+              <a href="https://www.youtube.com/channel/UCYAO5FhEuoJK-3HCy9XN74Q" class="p-3"><span class="fa fa-youtube-play"></span></a>
+            </p>
+          </div>
+         <div class="col-md-4">
+            <h3>Address</h3>
+            <p>Jalan Nusantara , Perum . Pujangga Alam Blok G2 No 2 , Labuhan Dalam , Bandar Lampung</p>
+          </div>
+		  </div>
+       <div class="row justify-content-center">
+          <div class="col-md-7 text-center">
+			<p> BuduBagak Team </p>
+            <p> Arlis Putra Prabowo Febryoga Saputra Haravy Egi Pratama </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- END footer -->
+    
+    <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+
+    <script src="js/main.js"></script>
+  </body>
+</html>
